8de69a1334de9c25e58af52ef7486b6ab98c30e7
Richard Sloman <github@richardsloman.co.uk>
2020-05-20 15:37:30 +0100

    Added more browsers

--- SeleniumFramework/Driver.cs
+++ SeleniumFramework/Driver.cs
@@ -1,23 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
 
 namespace SeleniumFramework
 {
     public static class Driver
     {
-        public static IWebDriver webDriver { get; set; }
+        public static IWebDriver webDriver;
 
-        public static void SetupBrowser(string browserType)
+        public static void SetupBrowser(string browserType, double timeoutInSeconds = 30)
         {
             switch (browserType.Trim().ToLower())
             {
                 case "chrome":
-                    webDriver = new ChromeDriver(".");
+                    webDriver = new ChromeDriver();
                     break;
+                case "ie":
+                    webDriver = new InternetExplorerDriver();
+                    break;
+                case "firefox":
+                    FirefoxDriverService service = FirefoxDriverService.CreateDefaultService();
+                    service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+                    webDriver = new FirefoxDriver(service);
+                    break;
+                default:
+                    throw new Exception("Unhandled browser name : " + browserType);
             }
+            webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(timeoutInSeconds);
+            webDriver.Manage().Window.Maximize();
         }
     }
 }
