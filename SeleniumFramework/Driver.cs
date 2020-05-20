b29f0457fe5198a8dce5d084bbf69b0009fa6eed
Richard Sloman <github@richardsloman.co.uk>
2020-05-20 13:54:17 +0100

    Add project files.

---
+++ SeleniumFramework/Driver.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumFramework
+{
+    public static class Driver
+    {
+        public static IWebDriver webDriver { get; set; }
+
+        public static void SetupBrowser(string browserType)
+        {
+            switch (browserType.Trim().ToLower())
+            {
+                case "chrome":
+                    webDriver = new ChromeDriver();
+                    break;
+            }
+        }
+    }
+}
